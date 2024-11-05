8fcf064822eef33d94cff2d4e4c64641eb7948c6
Gabriel Ayres <gbayres@hotmail.com>
2024-11-05 10:19:54 -0300

    transitions

--- app.vue
+++ app.vue
@@ -10,4 +10,13 @@
 
 <style>
 @import url("~/assets/css/main.css");
+.page-enter-active,
+.page-leave-active {
+  transition: all 0.4s;
+}
+.page-enter-from,
+.page-leave-to {
+  opacity: 0;
+  filter: blur(1rem);
+}
 </style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,9 @@
 import vuetify, { transformAssetUrls } from "vite-plugin-vuetify"; // https://nuxt.com/docs/api/configuration/nuxt-config
 
 export default defineNuxtConfig({
+  app: {
+    pageTransition: { name: 'page', mode: 'out-in' }
+  },
   compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
   ssr: true,
